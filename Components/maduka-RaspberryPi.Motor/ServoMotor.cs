ef4d20c1b5fdd14d2a0dd9ef061bc6496c2081b4
maduka <maduka_bor@hotmail.com>
2017-03-14 21:39:48 +0800

    Add Servo Motor Content

--- Components/maduka-RaspberryPi.Motor/ServoMotor.cs
+++ Components/maduka-RaspberryPi.Motor/ServoMotor.cs
@@ -6,7 +6,97 @@ using System.Threading.Tasks;
 
 namespace maduka_RaspberryPi.Motor
 {
-    class ServoMotor
+    using Windows.Devices.Gpio;
+    using Windows.System.Threading;
+    using System.Diagnostics;
+    using System.Threading;
+    using Windows.Foundation;
+
+    public class ServoMotor
     {
+        public GpioPin pin;
+        public GpioPinValue pinValue;
+
+        private IAsyncAction workItemThread;
+        public GpioController gpio;
+
+        public ServoMotor(int servoPin)
+        {
+            gpio = GpioController.GetDefault();
+            pin = gpio.OpenPin(servoPin);
+            pinValue = GpioPinValue.High;
+            pin.Write(pinValue);
+            pin.SetDriveMode(GpioPinDriveMode.Output);
+        }
+
+        public void PWM_R(int intAngle)
+        {
+            var stopwatch = Stopwatch.StartNew();
+            intAngle = (intAngle * 300 / 90);
+
+            workItemThread = Windows.System.Threading.ThreadPool.RunAsync(
+                    (source) =>
+                    {
+                        // setup, ensure pins initialized
+                        ManualResetEvent mre = new ManualResetEvent(false);
+                        mre.WaitOne(1000);
+
+                        ulong pulseTicks = ((ulong)(Stopwatch.Frequency) / 1000) * 2;
+                        ulong delta;
+                        var startTime = stopwatch.ElapsedMilliseconds;
+                        while (stopwatch.ElapsedMilliseconds - startTime <= intAngle)
+                        {
+                            pin.Write(GpioPinValue.High);
+                            ulong starttick = (ulong)(stopwatch.ElapsedTicks);
+                            while (true)
+                            {
+                                delta = (ulong)(stopwatch.ElapsedTicks) - starttick;
+                                if (delta > pulseTicks) break;
+                            }
+                            pin.Write(GpioPinValue.Low);
+                            starttick = (ulong)(stopwatch.ElapsedTicks);
+                            while (true)
+                            {
+                                delta = (ulong)(stopwatch.ElapsedTicks) - starttick;
+                                if (delta > pulseTicks * 10) break;
+                            }
+                        }
+                    }, WorkItemPriority.High);
+        }
+
+        public void PWM_L(int intAngle)
+        {
+            intAngle = (intAngle * 300 / 90);
+
+            var stopwatch = Stopwatch.StartNew();
+            workItemThread = Windows.System.Threading.ThreadPool.RunAsync(
+                    (source) =>
+                    {
+                        // setup, ensure pins initialized
+                        ManualResetEvent mre = new ManualResetEvent(false);
+                        mre.WaitOne(1000);
+
+                        ulong pulseTicks = ((ulong)(Stopwatch.Frequency) / 1000) * 2;
+                        ulong delta;
+                        var startTime = stopwatch.ElapsedMilliseconds;
+                        while (stopwatch.ElapsedMilliseconds - startTime <= intAngle)
+                        {
+                            pin.Write(GpioPinValue.High);
+                            ulong starttick = (ulong)(stopwatch.ElapsedTicks);
+                            while (true)
+                            {
+                                delta = starttick - (ulong)(stopwatch.ElapsedTicks);
+                                if (delta > pulseTicks) break;
+                            }
+                            pin.Write(GpioPinValue.Low);
+                            starttick = (ulong)(stopwatch.ElapsedTicks);
+                            while (true)
+                            {
+                                delta = (ulong)(stopwatch.ElapsedTicks) - starttick;
+                                if (delta > pulseTicks * 10) break;
+                            }
+                        }
+                    }, WorkItemPriority.High);
+        }
     }
 }
--- UWP/maduka-RaspberryPi.App/MainPage.xaml.cs
+++ UWP/maduka-RaspberryPi.App/MainPage.xaml.cs
@@ -19,6 +19,7 @@ using Windows.UI.Xaml.Navigation;
 namespace maduka_RaspberryPi.App
 {
     using maduka_RaspberryPi.Motor;
+    using System.Threading.Tasks;
 
     /// <summary>
     /// 可以在本身使用或巡覽至框架內的空白頁面。
@@ -27,13 +28,35 @@ namespace maduka_RaspberryPi.App
     {
         GpioController objGpioController = GpioController.GetDefault();
         StepMotor objMotor = null;
+        ServoMotor objServo = null;
 
         public MainPage()
         {
             this.InitializeComponent();
-            objMotor = new StepMotor(5, 6, 13, 26) { Sleep = 15, };
+            // objMotor = new StepMotor(5, 6, 13, 26) { Sleep = 15, };
+            // objServo = new ServoMotor(18);
         }
 
+        #region // Servo Motor
+        private void btnReverse_Click(object sender, RoutedEventArgs e)
+        {
+            // 使用ServoMotor
+            objServo.PWM_R(int.Parse(txtAngle.Text));
+        }
+
+        private void btnFoward_Click(object sender, RoutedEventArgs e)
+        {
+            // 使用ServoMotor
+            objServo.PWM_L(int.Parse(txtAngle.Text));
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        #endregion
+
+        #region // Step Motor
         private async void btnReverseStep_Click(object sender, RoutedEventArgs e)
         {
             await objMotor.TurnAsync(int.Parse(txtAngleStep.Text), Enums.TurnDirection.Right, Enums.DrivingMethod.WaveDrive);
@@ -48,5 +71,45 @@ namespace maduka_RaspberryPi.App
         {
             objMotor.Stop();
         }
+        #endregion
+
+        #region // Servo Motor Anglr
+        private async void btn0Angle_Click(object sender, RoutedEventArgs e)
+        {
+            using (var servo = new ServoMotorAngle(18))
+            {
+                await servo.Connect();
+
+                servo.SetPosition(0);
+                servo.AllowTimeToMove(1000);
+                servo.Go();
+            }
+        }
+
+        private async void btn90Angle_Click(object sender, RoutedEventArgs e)
+        {
+            // 讓馬達回到正中央,90度的地方
+            using (var servo = new ServoMotorAngle(18))
+            {
+                await servo.Connect();
+
+                servo.SetPosition(90);
+                servo.AllowTimeToMove(1000);
+                servo.Go();
+            }
+        }
+
+        private async void btn180Angle_Click(object sender, RoutedEventArgs e)
+        {
+            using (var servo = new ServoMotorAngle(18))
+            {
+                await servo.Connect();
+                servo.SetPosition(180);
+                servo.AllowTimeToMove(1000);
+                servo.Go();
+            }
+        }
+        #endregion
+
     }
 }
